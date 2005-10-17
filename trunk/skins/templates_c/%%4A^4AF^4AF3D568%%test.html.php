d35dab1952d3ebbe104b2d8ea41cd2d2fb03b6d2
fatihboy <fatihboy@e2194efc-cfd4-0310-9665-f6a9b7687b2b>
2005-10-17 17:44:41 +0000

    removed unused template files

--- trunk/skins/templates_c/%%4A^4AF^4AF3D568%%test.html.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.7, created on 2005-02-24 17:39:25
-         compiled from F:%5CBackUp%5CMy+Documents%5CMy+Projects%5Centerprisecoding.com%5CAlumni-Online%5CSource+Files%5Ccontrols%5Ccustomcontrols/test.html */ ?>
-<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
-smarty_core_load_plugins(array('plugins' => array(array('function', 'aoForms_TextField', 'D:\\BackUp\\My Documents\\My Projects\\enterprisecoding\\Alumni-Online\\Source Files\\controls\\customcontrols/test.html', 1, false),)), $this); ?>
-<?php echo smarty_function_aoForms_TextField(array('name' => 'deneme1'), $this);?>
-deneme
